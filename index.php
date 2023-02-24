fa5762efa6e65385e92ccfc178894f0b23588010
Brooklyn <101115866+Brooklyn2309@users.noreply.github.com>
2023-02-24 13:50:20 +0100

    Update

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <title>Login</title>
   </head>
   <body>
-    <h1>Login om verder te gaan</h1><br>
+    <h1>Login om verder te gaan.</h1><br>
 
     <p>als u nieuw bent moet een nieuwe account aan maken</p><br><br>
     <p><a href="login.php">Login</p><br><br>
